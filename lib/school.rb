094b11b7632b73d7d39b088da3404656e8837771
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-12 17:53:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,9 +6,9 @@ class School
     @roster = {}
   end
     
-  def add_student(student, grade)
-    @roster[grade] ||= []
-    @roster[grade] << 
+  def add_student(student_name, student_grade)
+    @roster[student_grade] ||= []
+    @roster[student_name] << student_name
   end
       
   
